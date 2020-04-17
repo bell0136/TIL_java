0fc45d831bf0380b2e2c99e2ac5d41fc717eb5e0
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-04-17 15:00:07 +0900

    2020_04_17

--- C04/ex4_1pro5.java
+++ C04/ex4_1pro5.java
@@ -2,10 +2,10 @@ class ex4_1pro5
 {
 	public static void main(String[] args)
 	{
-		int A = ((25+5)+(36/4)-72)*5;
-		int B = ((25+5)+(36+4)+71)/4;
-		int C = ((128/4)*2);
-		if(A>B&&B>C&&A>C)
+		int A = ((25 + 5) + (36 / 4) - 72) * 5;
+		int B = ((25 + 5) + (36 + 4) + 71) / 4;
+		int C = ((128 / 4) *2);
+		if(A>B && B>C && A>C)
 		{
 			System.out.println("true");
 		}
--- C06/ex6_3pro2.java
+++ C06/ex6_3pro2.java
@@ -2,17 +2,18 @@ class ex6_3pro2
 {
 	public static void main(String[] args)
 	{		
-		int num = 8;
-		Binary(8);
+		int num = 4;
+		System.out.println(Binary(num));
+		
 	}
-	public static int Binary(int num)
+	public static String Binary(int num)
 	{	
 		if(num == 1)
 		{
-			System.out.println(num);
-			return 0; 
+			//System.out.println(num);
+			return "1";
 		}
-		System.out.println(num%2);
-		return 2*Binary(num/2);
+		//System.out.println(num%2);
+		return Binary(num/2)+(num%2);
 	}
 }

b7f504aab6c5b6d0be608eefaf9f4da7109421fc
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-05-27 19:10:11 +0900

    2020_05_27

--- C07/ex7_1pro2.java
+++ C07/ex7_1pro2.java
@@ -1,3 +1,5 @@
+package ex7_1pro2;
+
 class beads
 {
 	int beads;
@@ -8,12 +10,14 @@ class beads
 	public void plus_beads(int Beads, beads child)
 	{
 		beads += Beads;
+		System.out.println("************************");
 		System.out.println("구슬"+Beads+"개를 받았습니다.");
 		child.minus_beads(Beads);		
 	}
 	public void minus_beads(int Beads)
 	{
 		beads -= Beads;
+		
 		System.out.println("구슬"+Beads+"개를 줬습니다.");
 			
 	}
@@ -27,13 +31,21 @@ class ex7_1pro2
 	public static void main(String[] args)
 	{
 		beads child1 = new beads(15);		
-		beads child2 = new beads(2);
-		System.out.println("게임전");
+		beads child2 = new beads(13);
+		System.out.println("게임 전 보유현황");
 		System.out.println("child1's beads count is "+child1.showBeads());
 		System.out.println("child2's beads count is "+child2.showBeads());	
 		child1.plus_beads(2,child2);
-		System.out.println("게임후");
+		System.out.println("1차 게임 후 구슬 보유현황");
 		System.out.println("child1's beads count is "+child1.showBeads());
 		System.out.println("child2's beads count is "+child2.showBeads());		
+		child2.plus_beads(5,child1);
+		System.out.println("2차 게임 후 구슬 보유현황");
+		System.out.println("child1's beads count is "+child1.showBeads());
+		System.out.println("child2's beads count is "+child2.showBeads());	
+		
 	}	
 }
+
+
+

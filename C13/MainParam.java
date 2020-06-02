b81265b29c87dc7fe06d8c2f35e4edda4176d163
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-06-02 11:15:02 +0900

    2020_0602

---
+++ C13/MainParam.java
@@ -0,0 +1,14 @@
+public class MainParam
+{	
+	public static void main(String[] args) 
+	{
+		
+		for(String index : args)
+		{
+
+			System.out.printf(index+" ");
+		}
+		System.out.println();
+		
+	}
+}
